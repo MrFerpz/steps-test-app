a61dcde3473d2cad24c415a587c67bb65d150da6
Fred <leferpz@gmail.com>
2024-12-22 19:02:14 -0800

    Reversed failed clip path experiments

--- src/components/Goal.jsx
+++ src/components/Goal.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 export default function Goal({goalTitle}) {
     return (
-        <div className="goal-container">{goalTitle}
+        <div className="goals-container">{goalTitle}
             <div className="box">
                 <div className="box-dynamic-fill"></div>
             </div>
